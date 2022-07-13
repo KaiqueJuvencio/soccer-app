e2724b8acb0715152c0c9e4c5cb199007b806b4f
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2022-07-13 20:19:52 -0300

    :dart: Add StatisticsDTO

---
+++ src/main/java/com/br/soccerapp/model/StatisticsDTO.java
@@ -0,0 +1,26 @@
+package com.br.soccerapp.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "STATISTICS")
+@Data
+public class StatisticsDTO {
+
+    @Id
+    private String id;
+    @ManyToOne
+    @OneToOne
+    private LeagueDTO league;
+    @ManyToOne
+    @OneToOne
+    private TeamDTO team;
+    private Integer victories;
+    private Integer defeats;
+    private Integer goalDifference;
+    private Integer points;
+    private Integer matchesQuantities;
+}
