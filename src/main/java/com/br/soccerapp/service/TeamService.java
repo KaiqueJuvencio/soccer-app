0a4b34da8813796ac7f5067e06567df01352822c
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2023-02-12 17:15:17 -0300

    :dart: Add exceptions on TeamService

--- src/main/java/com/br/soccerapp/service/TeamService.java
+++ src/main/java/com/br/soccerapp/service/TeamService.java
@@ -1,5 +1,7 @@
 package com.br.soccerapp.service;
 
+import com.br.soccerapp.exception.BadRequestException;
+import com.br.soccerapp.exception.ObjectNullException;
 import com.br.soccerapp.model.LeagueDTO;
 import com.br.soccerapp.model.TeamDTO;
 import com.br.soccerapp.repository.LeagueRepository;
@@ -25,26 +27,44 @@ public class TeamService {
     }
 
     public TeamDTO create(String name, Long leagueId){
-        Optional<LeagueDTO> league = leagueRepository.findById(leagueId);
-        if(league.isPresent()){
+        try{
+            Optional<LeagueDTO> league = leagueRepository.findById(leagueId);
+            this.verifyIsNull(league);
             TeamDTO teamDTO = new TeamDTO(name, league.get());
             return teamRepository.save(teamDTO);
-        }else {
-            throw new RuntimeException();
+        }catch (ObjectNullException e){
+            throw new ObjectNullException("League not exist");
+        }catch (Exception e) {
+            throw new BadRequestException("Error to create Team");
         }
     }
 
     public void update(String name, Long teamId){
-        Optional<TeamDTO> team = teamRepository.findById(teamId);
-        if(team.isPresent()){
+        try{
+            Optional<TeamDTO> team = teamRepository.findById(teamId);
+            this.verifyIsNull(team);
             team.get().setName(name);
             teamRepository.save(team.get());
-        }else {
-            throw new RuntimeException();
+        }catch (ObjectNullException e){
+            throw new ObjectNullException("Team not exist");
+        }catch (Exception e) {
+            throw new BadRequestException("Error to update Team");
         }
     }
 
     public void delete(Long id){
-        teamRepository.deleteById(id);
+        try {
+            Optional<TeamDTO> team = teamRepository.findById(id);
+            this.verifyIsNull(team);
+            teamRepository.deleteById(id);
+        }catch (ObjectNullException e){
+            throw new ObjectNullException("Team not exist");
+        }catch (Exception e) {
+            throw new BadRequestException("Error to delete Team");
+        }
+    }
+
+    public void verifyIsNull(Optional<?> method){
+        if(method.isEmpty()) throw new ObjectNullException("");
     }
 }
