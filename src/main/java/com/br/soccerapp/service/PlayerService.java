a1614d961a32fb4f69b99f86a606523cd4380785
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2023-05-28 20:18:27 -0300

    :dart: Improving PlayerService code

--- src/main/java/com/br/soccerapp/service/PlayerService.java
+++ src/main/java/com/br/soccerapp/service/PlayerService.java
@@ -1,22 +1,19 @@
 package com.br.soccerapp.service;
 
-import com.br.soccerapp.exception.BadRequestException;
 import com.br.soccerapp.exception.ObjectNullException;
 import com.br.soccerapp.model.entity.Player;
 import com.br.soccerapp.model.entity.Team;
 import com.br.soccerapp.repository.PlayerRepository;
-import com.br.soccerapp.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PlayerService {
 
     @Autowired
-    TeamRepository teamRepository;
+    TeamService teamService;
 
     @Autowired
     PlayerRepository playerRepository;
@@ -25,48 +22,25 @@ public class PlayerService {
         return playerRepository.findAll();
     }
 
-    public Player create(String name, Long teamId){
-        try{
-            Optional<Team> team = teamRepository.findById(teamId);
-            this.verifyIsNull(team);
-            Player player = new Player(name, team.get());
-            return playerRepository.save(player);
-        }catch (ObjectNullException e){
-            throw new ObjectNullException("Team not exist");
-        }catch (Exception e){
-            throw new BadRequestException("Error to create Player");
-        }
+    public Player findById(Long id){
+        return playerRepository.findById(id).orElseThrow(()-> new ObjectNullException("Player not exist"));
+    }
+
+    public void create(String name, Long teamId){
+        Team team = teamService.findById(teamId);
+        playerRepository.save(new Player(name, team));
     }
 
     public void update(String name, Long playerId, Long teamId){
-        try{
-            Optional<Player> player = playerRepository.findById(playerId);
-            this.verifyIsNull(player);
-            Optional<Team> team = teamRepository.findById(teamId);
-            this.verifyIsNull(team);
-            player.get().setName(name);
-            player.get().setTeam(team.get());
-            playerRepository.save(player.get());
-        }catch (ObjectNullException e){
-            throw new ObjectNullException("Team or Player not exist");
-        }catch (Exception e) {
-            throw new BadRequestException("Error to update Player");
-        }
+        Player player = this.findById(playerId);
+        Team team = teamService.findById(teamId);
+        player.setName(name);
+        player.setTeam(team);
+        playerRepository.save(player);
     }
 
     public void delete(Long id){
-        try{
-            Optional<Player> player = playerRepository.findById(id);
-            this.verifyIsNull(player);
-            playerRepository.deleteById(id);
-        }catch (ObjectNullException e){
-            throw new ObjectNullException("Player not exist");
-        }catch (Exception e) {
-            throw new BadRequestException("Error to delete Player");
-        }
-    }
-
-    public void verifyIsNull(Optional<?> method){
-        if(method.isEmpty()) throw new ObjectNullException("");
+        this.findById(id);
+        playerRepository.deleteById(id);
     }
 }
